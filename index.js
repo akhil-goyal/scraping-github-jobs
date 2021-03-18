e65f22af20b1af8949741721378f580a72548666
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-18 18:39:07 -0400

    Added the functionality to fetch all the jobs from all pages of gthub jobs api & store them in excel sheet.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Sheet = require('./sheet');
 const fetch = require('node-fetch');
 
-(async function () {
+async function scrapePage(i) {
 
-    const res = await fetch(`https://jobs.github.com/positions.json?location=remote`);
+    const res = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=code`);
     const json = await res.json();
 
     const rows = json.map(job => {
@@ -14,7 +14,24 @@ const fetch = require('node-fetch');
             date: job.created_at,
             url: job.url
         }
-    })
+    });
+
+    return rows;
+
+}
+
+(async function () {
+
+    let i = 1;
+    let rows = [];
+
+    while(true){
+        const newRows = await scrapePage(i);
+        if(newRows.length === 0) break;
+        rows = rows.concat(newRows);
+        // rows = [...rows, ...newRows];
+        i++;
+    } 
 
     const sheet = new Sheet();
     await sheet.load();
